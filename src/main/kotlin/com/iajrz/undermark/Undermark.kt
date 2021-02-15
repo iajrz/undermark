a55a8a684259b69293e859813e4c5d5a8e8d65ae
Irving A.J. Rivas Z <axel.rivas@gmail.com>
2021-02-15 19:07:21 -0400

    changes the parsing logic, isolating different parsing modes in classes

--- src/main/kotlin/com/iajrz/undermark/Undermark.kt
+++ src/main/kotlin/com/iajrz/undermark/Undermark.kt
@@ -2,73 +2,85 @@ package com.iajrz.undermark
 
 class UnderMark(private val string: String) {
 
-    enum class State {
-        NAKED_DOCUMENT, PARAGRAPH, HEADER, HEADER_START
+    fun parse(): String {
+        val parser = NakedDocument(ParseableDoc(string))
+        parser.parse()
+        return parser.doc.result
+    }
+}
+
+class ParseableDoc(val src: String, var cursor: Int = -1, var result: String = "") {
+    fun peekNext(): Char {
+        return src[cursor + 1]
     }
 
-    private var state = ArrayDeque<State>()
-    private var cursor = 0
+    fun next(): Char {
+        cursor++
+        return src[cursor]
+    }
 
-    fun parse(): String {
-        var r = ""
-        state.addFirst(State.NAKED_DOCUMENT)
+    fun hasNext(): Boolean {
+        return (cursor + 1) < src.length
+    }
+}
 
-        while (cursor < string.length) {
-            val c = string[cursor]
-            when (state.first()) {
-                State.NAKED_DOCUMENT -> {
-                    when (c) {
-                        '#' -> {
-                            state.addFirst(State.HEADER_START)
-                        }
-                        else -> {
-                            r += "<p>$c"
-                            state.addFirst(State.PARAGRAPH)
-                        }
-                    }
+abstract class UndermarkParser(var doc: ParseableDoc) {
+    abstract fun parse()
+}
+
+class NakedDocument(doc: ParseableDoc) : UndermarkParser(doc) {
+    private var isStartOfLine = true
+
+    override fun parse() {
+        while (doc.hasNext()) {
+            val currentCharacter = doc.peekNext()
+
+            if (isStartOfLine) {
+                if (currentCharacter == '#') {
+                    Header(doc).parse()
+                } else if (currentCharacter != '\n') {
+                    Paragraph(doc).parse()
                 }
-                State.HEADER_START -> {
-                    if (c != ' ') {
-                        r += "<h1>$c"
-                        state.removeFirst()
-                        state.addFirst(State.HEADER)
-                    }
-                }
-                State.HEADER -> {
-                    when (c) {
-                        '\n' -> {
-                            r += "</h1>"
-                            state.removeFirst()
-                        }
-                        else -> {
-                            r += c
-                        }
-                    }
-                }
-                State.PARAGRAPH -> {
-                    if (c == '\n') {
-                        r += "</p>"
-                        state.removeFirst()
-                    } else {
-                        r += c
-                    }
-                }
+                isStartOfLine = false
+                continue // every time we "parse", we should skip the rest of the current iteration!
             }
-            cursor++
-        }
-        while (state.size > 0) {
-            when (state.first()) {
-                State.PARAGRAPH -> {
-                    r += "</p>"
-                }
-                State.HEADER -> {
-                    r += "</h1>"
-                }
-                else -> {
-                }
+
+            isStartOfLine = currentCharacter == '\n'
+            if (currentCharacter == '\n') {
+                doc.next()
+            } else {
+                doc.result += doc.next()
             }
-            state.removeFirst()
         }
-        return r
+    }
+}
+
+class Paragraph(doc: ParseableDoc) : UndermarkParser(doc) {
+    override fun parse() {
+        doc.result += "<p>"
+        while (doc.hasNext() && doc.peekNext() != '\n') {
+            doc.result += doc.next()
+        }
+        doc.result += "</p>"
+    }
+
+}
+
+class Header(doc: ParseableDoc) : UndermarkParser(doc) {
+    override fun parse() {
+        var depth = 0
+        while (doc.hasNext() && doc.peekNext() == '#' && depth < 6) {
+            depth++
+            doc.next()
+        }
+        doc.result += "<h${depth}>"
+        while (doc.hasNext() && doc.peekNext() == ' ') {
+            doc.next()
+        }
+
+        while (doc.hasNext() && doc.peekNext() != '\n') { // this should be interruptible by * and ** besides \n
+            doc.result += doc.next()
+        }
+        doc.result += "</h${depth}>"
     }
 }
